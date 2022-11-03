27cecb12b524fde4a8638ec4a2c4f5246e75d4df
VilmaTuresson <vilma.turesson@gmail.com>
2022-11-03 13:14:55 +0000

    Add more style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar, Container, Nav, NavDropdown, FormControl, Form, Button} from 'r
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar expand="lg" fixed="top">
       <Container>
         <Navbar.Brand className={styles.Logo}>TestHub</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -14,15 +14,15 @@ const NavBar = () => {
             style={{ maxHeight: '500px' }}
             navbarScroll
           >
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Account</Nav.Link>
-            <NavDropdown title="Posts" id="navbarScrollingDropdown">
+            <Nav.Link className={styles.NavItem}>Home</Nav.Link>
+            <Nav.Link className={styles.NavItem}>Account</Nav.Link>
+            <NavDropdown className={styles.NavItem} title="Posts" id="navbarScrollingDropdown">
               <NavDropdown.Item>My Posts</NavDropdown.Item>
               <NavDropdown.Item>Liked Posts</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item>Create new Post</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link>
+            <Nav.Link className={styles.NavItem}>
               Logout
             </Nav.Link>
           </Nav>
